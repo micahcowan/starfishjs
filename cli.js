65e82526e3e9e384e7a26035f29fa6935f7c2185
Micah Cowan <micah@addictivecode.org>
2017-08-02 02:09:35 -0700

    cli.js cleanup.

--- cli.js
+++ cli.js
@@ -1,32 +1,53 @@
 #!/usr/bin/env node
 
-const Starfish = require('./starfish.js').starfish;
+let Starfish = require('./starfish.js').starfish;
 let PNG = require('node-png').PNG;
 let png = new PNG({width: 384, height: 384, filterType: -1});
 let fs = require('fs');
+let util = require('util');
 
-const generators = [
-    './sf-bubble.js'
-  , './sf-coswave.js'
-  , './sf-flatwave.js'
-  , './sf-rangefrac.js'
-  , './sf-spinflake.js'
-];
+let stdout = process.stdout;
+let outFName = 'output.png';
+let outFile = fs.createWriteStream(outFName)
+outFile.on('error', handleError);
+outFile.on('open', startRender);
 
-for (let gen of generators) {
-    let mod = require(gen);
-    mod.registerLayer(Starfish);
+// Done.
+
+function handleError(e) {
+    console.error(e);
+    process.exit(1);
 }
 
-let inst = new Starfish.Instance;
-
-inst.render(png
-  , (kw) => {
-        console.log(`PROGRESS: ${kw.curLayer} / ${kw.numLayers}: ${kw.percentDone} %\n`);
+function startRender() {
+    const generators = [
+        './sf-bubble.js'
+      , './sf-coswave.js'
+      , './sf-flatwave.js'
+      , './sf-rangefrac.js'
+      , './sf-spinflake.js'
+    ];
+    for (let gen of generators) {
+        let mod = require(gen);
+        mod.registerLayer(Starfish);
     }
-  , () => {
-      png.on(
-        'end', () => { console.log("foo"); });
-      png.pack().pipe(fs.createWriteStream('out.png'));
-  }
-);
+
+    let inst = new Starfish.Instance;
+
+    inst.render(png, handleRenderProgress, writeToImageFile);
+}
+
+function handleRenderProgress(kw) {
+    stdout.write(`\rPROGRESS: Layer ${kw.curLayer} / ${kw.numLayers}: ${kw.percentDone} %          `);
+}
+
+function writeToImageFile() {
+    stdout.write("\rPROGRESS: Done.                   \n");
+    png.on('end'
+           , () => {
+               console.log("Finished writing " + util.inspect(outFName) +".");
+           }
+          );
+    png.on('error', handleError);
+    png.pack().pipe(outFile);
+}
