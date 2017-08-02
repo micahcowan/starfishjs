dbe063e612765cd5b402fa76c352b9faf4cf206c
Micah Cowan <micah@addictivecode.org>
2017-08-02 01:34:46 -0700

    Basic PNG creation.

--- cli.js
+++ cli.js
@@ -1,4 +1,10 @@
+#!/usr/bin/env node
+
 const Starfish = require('./starfish.js').starfish;
+let PNG = require('node-png').PNG;
+let png = new PNG({width: 384, height: 384, filterType: -1});
+let fs = require('fs');
+
 const generators = [
     './sf-bubble.js'
   , './sf-coswave.js'
@@ -13,3 +19,14 @@ for (let gen of generators) {
 }
 
 let inst = new Starfish.Instance;
+
+inst.render(png
+  , (kw) => {
+        console.log(`PROGRESS: ${kw.curLayer} / ${kw.numLayers}: ${kw.percentDone} %\n`);
+    }
+  , () => {
+      png.on(
+        'end', () => { console.log("foo"); });
+      png.pack().pipe(fs.createWriteStream('out.png'));
+  }
+);
